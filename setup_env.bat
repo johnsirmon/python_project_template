d79e7e2197ce6584d78966372e844a0226d0bd40
John Sirmon <johnsirmon@hotmail.com>
2023-12-15 06:30:54 -0500

    Update setup_env.bat

--- setup_env.bat
+++ setup_env.bat
@@ -1,10 +1,19 @@
 @echo off
 SETLOCAL ENABLEDELAYEDEXPANSION
 
+REM Check for administrative privileges
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    echo You must run this script as an Administrator.
+    echo Right-click the script and choose "Run as administrator".
+    exit /b 1
+)
+
 REM Check for Conda installation
 conda --version > nul 2>&1
 if %errorlevel% neq 0 (
     echo Conda is not installed or not in PATH.
+    echo Please install Conda and ensure it's added to your PATH.
     exit /b 1
 )
 
@@ -14,10 +23,20 @@ SET /P PYTHON_VERSION="Enter Python version (e.g., 3.8): "
 REM Create Conda environment
 echo Creating Conda environment...
 conda create --name myenv python=%PYTHON_VERSION% -y
-IF %ERRORLEVEL% NEQ 0 EXIT /b %ERRORLEVEL%
+IF %ERRORLEVEL% NEQ 0 (
+    echo Failed to create Conda environment.
+    echo This might be due to an incorrect Python version or a network issue.
+    echo Please check your Python version and internet connection.
+    EXIT /b %ERRORLEVEL%
+)
 
 REM Activate Conda environment
 CALL activate myenv
+IF %ERRORLEVEL% NEQ 0 (
+    echo Failed to activate the Conda environment.
+    echo This might be because the environment was not created correctly.
+    EXIT /b %ERRORLEVEL%
+)
 
 REM Create project directories
 echo Creating project directories...
